c31530c7313b8c7c3610c00961c5cc010eb98141
MARKST-47 <markstephenthomas10@gmail.com>
2024-10-06 15:59:50 +0530

    Hover func

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -4,13 +4,19 @@ from rxconfig import config
 
 class State(rx.State):
     """The app state."""
-    count: int = 0
 
-    def increment(self):
-        self.count += 1
-    
-    def decrement(self):
-        self.count -= 1
+    count: int = 0
+    label: str = "Welcome to Reflex!"
+    color: str = "green"
+
+    def handle_title_change(self, val):
+        self.label = val
+
+    def change_color(self):
+        self.color = "blue"
+
+    def reset_color(self):
+        self.color = "green"
 
 
 def index() -> rx.Component:
@@ -18,32 +24,29 @@ def index() -> rx.Component:
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
+            rx.heading(State.label, size="9"),
             rx.text(
-                "Get started by editing ",
+                "Get started by editing.",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
+            ),
+            rx.input(
+                default_value=State.label,
+                on_change=State.handle_title_change,
+            ),
+            rx.button(
+                "COLOR",
+                style={"background-color": State.color},
+                on_mouse_over=State.change_color,
+                on_mouse_out=State.reset_color,
             ),
             spacing="5",
             justify="center",
             min_height="85vh",
         ),
-        rx.hstack(
-            rx.button(
-                'Decrement',
-                color_scheme='red',
-                on_click=State.decrement,
-            ),
-            rx.heading(State.count, font_size="2em"),
-            rx.button(
-                "Increment",
-                color_scheme="green",
-                on_click=State.increment,
-            ),
-            spacing="4",
-        ),
         rx.logo(),
     )
+
 
 app = rx.App()
 

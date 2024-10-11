bc228d411a3a0a4f9e92fa11ce8459236b78f0b9
MARKST-47 <markstephenthomas10@gmail.com>
2024-10-11 15:39:14 +0530

    signup, login changes

--- full_stack_python/pages/__init__.py
+++ full_stack_python/pages/__init__.py
@@ -2,10 +2,14 @@ from .about import about_page
 from .pricing import pricing_page
 from .contact import contact_page
 from .todo import todo_page
+from .signup import signup_page
+from .login import login_page
 
 __all__ = [
     'about_page',
     'pricing_page',
     'contact_page',
     'todo_page',
+    'signup_page',
+    'login_page',
 ]
